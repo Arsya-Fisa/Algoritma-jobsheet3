e7338b6be1a9e1dc45aad3a15b15e4a9b39b422e
Arsya-Fisa <Arsya Fikri S>
2025-02-27 10:06:32 +0700

    percobaan 2

---
+++ MahasiswaDemo01.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class MahasiswaDemo01 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Mahasiswa01 [] arrayofMahasiswa = new Mahasiswa01 [3];
+        String dummy;
+        for (int i = 0; i < 3; i++) {
+            arrayofMahasiswa[i] = new Mahasiswa01();
+            System.out.println("Masukan Data Mahasiswa ke-" + (i+1));
+            System.out.print("NIM: ");
+            arrayofMahasiswa[i].nim = sc.nextLine();
+            System.out.print("Nama: ");
+            arrayofMahasiswa[i].nama = sc.nextLine();
+            System.out.print("Kelas: ");
+            arrayofMahasiswa[i].kelas= sc.nextLine();
+            System.out.print("IPK: ");
+            dummy = sc.nextLine();
+            arrayofMahasiswa[i].ipk  = Float.parseFloat(dummy);
+            System.out.println("--------------------------");
+        }
+        for (int i = 0; i < 3; i++) {
+           System.out.println("Mahasiswa ke-" + (i+1)+ ": ");
+           arrayofMahasiswa[i].cetakInfo();
+        }
+       
+    }
+}
