04adf50f63ce56d5e2ff4a10065c3c69c032babc
Arsya-Fisa <Arsya Fikri S>
2025-02-27 11:23:38 +0700

    percobaan 3

--- Matakuliah01.java
+++ Matakuliah01.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Matakuliah01 {
     public String kode;
     public String nama;
@@ -10,5 +12,23 @@ public class Matakuliah01 {
         this.sks = sks;
         this.jumlahJam = jumlahJam;
     }
-    
+    public void tambahdata(){
+        Scanner sc = new Scanner (System.in);
+        System.out.print("Kode: ");
+        this.kode = sc.nextLine();
+        System.out.print("Nama: ");
+        this.nama = sc.nextLine();
+        System.out.print("SKS: ");
+        this.sks = Integer.parseInt(sc.nextLine());
+        System.out.print("Jumlah jam: ");
+        this.jumlahJam = Integer.parseInt(sc.nextLine());
+        System.out.println("-----------------------------");
+    }
+    public void cetakinfo(){
+        System.out.println("Kode         : " + kode);
+        System.out.println("Nama         : " + nama);
+        System.out.println("SKS          : " + sks);
+        System.out.println("Jumlah jam   : " + jumlahJam);
+        System.out.println("-----------------------------------");
+    }
 }
--- MatakuliahDemo01.java
+++ MatakuliahDemo01.java
@@ -2,33 +2,18 @@ import java.util.Scanner;
 public class MatakuliahDemo01 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        Matakuliah01 [] arrayofMatakuliah01 = new Matakuliah01[3];
-        String kode,nama,dummy;
-        int sks, jumlahJam;
+        System.out.print("Masukan jumlah Mata kuliah: ");
+        int jumlahMatkul = sc.nextInt();
+        Matakuliah01 [] arrayofMatakuliah01 = new Matakuliah01[jumlahMatkul];
         
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < jumlahMatkul; i++) {
             System.out.println("Masukan Data kuliah ke-" + (i+1));
-            System.out.print("Kode: ");
-            kode = sc.nextLine();
-            System.out.print("Nama: ");
-            nama = sc.nextLine();
-            System.out.print("SKS: ");
-            dummy  = sc.nextLine();
-            sks = Integer.parseInt(dummy);
-            System.out.print("Jumlah jam: ");
-            dummy = sc.nextLine();
-            jumlahJam = Integer.parseInt(dummy);
-            System.out.println("-----------------------------");
-
-            arrayofMatakuliah01[i] = new Matakuliah01(kode, nama, sks, jumlahJam);
+           arrayofMatakuliah01[i] = new Matakuliah01("", "", 0, 0);
+           arrayofMatakuliah01[i].tambahdata();
         }
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < jumlahMatkul; i++) {
             System.out.println("Data Matakuliah ke-" + (i+1));
-            System.out.println("Kode: " + arrayofMatakuliah01[i].kode);
-            System.out.println("Nama: " + arrayofMatakuliah01[i].nama);
-            System.out.println("SKS: " + arrayofMatakuliah01[i].sks);
-            System.out.println("Jumlah jam: " + arrayofMatakuliah01[i].jumlahJam);
-            System.out.println("---------------------------------------------");
+            arrayofMatakuliah01[i].cetakinfo();
         }
     }
 }
